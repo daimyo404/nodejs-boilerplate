833233c98314fe540715027df1f9b70dbaf362dd
斉藤大明 <saito-hiroaki@saitoudaiminnoMacBook-Air.local>
2024-01-25 17:00:56 +0900

    [feat]ディレクトリ名称をapiに変更

--- src/tests/api/jsonplaceholder.test.ts
+++ src/tests/api/jsonplaceholder.test.ts
@@ -1,8 +1,9 @@
-import { fetchUsers } from '../../services/jsonplaceholder';
+import { fetchUsers } from '../../api/jsonplaceholder';
 
 describe('jsonplaceholder', () => {
   it('should return users', async () => {
     const users = await fetchUsers();
+    console.log(users);
     expect(users).toBeDefined();
   });
 });
