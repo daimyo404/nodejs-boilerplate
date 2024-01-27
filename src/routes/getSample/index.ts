befc49ba478a8040c2f559d70ca34162ec51ad80
斉藤大明 <saito-hiroaki@saitoudaiminnoMacBook-Air.local>
2024-01-27 22:55:31 +0900

    [feat]/のレスポンス修正

--- src/routes/getSample/index.ts
+++ src/routes/getSample/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { fetchUsers } from '../../api/jsonplaceholder';
 
-export const getSample = async (_req: Request, _res: Response) => {
+export const getSample = async (_req: Request, res: Response) => {
   const users = await fetchUsers();
-  return users;
+  return res.json(users);
 };
